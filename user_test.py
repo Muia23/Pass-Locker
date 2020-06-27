491ba7778d425f25e2007969bbef69a142363ecd
Muia23 <wilfred.muema@gmail.com>
2020-06-27 12:26:02 +0300

    add a set up method for the test class

--- user_test.py
+++ user_test.py
@@ -2,3 +2,21 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
+
+    '''
+
+    Defines test cases for the user class behaviours.
+
+    '''
+
+def setUp(self):
+    '''
+    
+    Method runs before each test.
+
+    '''
+    self.new_user = User("Wilfred", "wil1234")
+
+
+if __name__ == '__main__':
+    unittest.main()
