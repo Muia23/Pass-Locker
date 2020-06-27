8a8f1da151bfc2054891700678841aaaab45af36
Muia23 <wilfred.muema@gmail.com>
2020-06-27 11:55:45 +0300

    made a user class

---
+++ user.py
@@ -0,0 +1,20 @@
+class User:
+    """
+
+    Class generates new instance of users
+    
+    """
+
+
+    def __init__(self,username, password, account):
+
+        """
+
+        __init__ method helps us define properties for out object.
+
+        Args:
+            username : New user name.
+            password : New password.
+            account : New account.
+
+        """
